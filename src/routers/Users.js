8e6d28b6f8b1926176c617b1a4e25be770291b87
286692865@qq.com <286692865@qq.com>
2021-01-26 11:01:30 +0800

    密码加密

--- src/routers/Users.js
+++ src/routers/Users.js
@@ -1,6 +1,7 @@
 //登陆 注册
 const express = require("express");
 const users = express.Router();
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/users.js");
 
@@ -8,6 +9,12 @@ users.get("/test",(req,res) =>{
 	res.send({msg:"测试"})
 })
 
+// 登录
+users.post("/login",(req,res) =>{
+	
+})
+
+// 注册
 users.post("/register",(req,res) =>{
 	const now = new Date();
 	const userData = {
@@ -20,9 +27,15 @@ users.post("/register",(req,res) =>{
 	//存之前 先找
 	User.findOne({ where: {email: req.body.email }}).then((user) =>{
 		if(!user){
-			User.create(userData).then((user) =>{
-				console.log(user);
-				res.json("注册成功");
+			// 加密
+			bcrypt.hash(req.body.password,10, (err,hash) =>{
+				userData.password = hash;
+				User.create(userData).then((user) =>{
+					console.log(user);
+					res.json("注册成功");
+				}).catch((err) =>{
+					res.send("error" + err);
+				})
 			})
 		}else{
 			res.json("数据已存在");
