638e754efa40dad9639fae9024c676c9434e88b0
286692865@qq.com <286692865@qq.com>
2021-03-11 17:29:55 +0800

    校验

--- src/validation/login.js
+++ src/validation/login.js
@@ -4,8 +4,8 @@ const isEmpty = require("../until/is-empty");
 module.exports = function validateLoginInput(data) {
 	let errors = {};
 	
-	if (!Validator.isLength(data.name, {min: 2,max: 20})){
-		errors.name = "名字长度不能小于2位并且不能大于20位！";
+	if (!Validator.isEmail(data.email)){
+		errors.email = "邮箱不合法！";
 	}
 	
 	return{
